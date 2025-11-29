c53a47b67f7870a97827dbb756b76fd89a9b1ac0
WaasilaAsif <wasif.bscs24seecs@seecs.edu.pk>
2025-11-29 12:10:16 +0500

    Merged with Zainab

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
+import db from "./config/dbconfig.js"
+import cors from "cors";
+dotenv.config();
+const app = express();
+app.use(express.json());
+db();
+app.use(cors());
+
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
+});
