78fe267579d327e446b99dea456cfbfa798a4efc
WaasilaAsif <wasif.bscs24seecs@seecs.edu.pk>
2025-11-29 11:05:57 +0500

    Created some backend modules

---
+++ backend/config/dbconfig.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI,
+      {
+        dbName: 'myDatabase', 
+        useNewUrlParser: true,     
+        useUnifiedTopology: true,  
+      }
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`MongoDB connection error: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
